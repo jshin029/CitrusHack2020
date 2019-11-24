945dff629bb56fa6ce328f8a9fe82a8bc8dc14ed
John Shin <jshin029@ucr.edu>
2019-11-23 18:40:33 -0800

    Updates

---
+++ precache-manifest.bd325499283b5cdc5d97f297489803f0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b7af9d6097c6073d384def3b56f2b2bf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e92978012ee8f309fdfa",
+    "url": "/static/css/main.850a195a.chunk.css"
+  },
+  {
+    "revision": "77fc41e91889234c3976",
+    "url": "/static/js/2.54e000c7.chunk.js"
+  },
+  {
+    "revision": "e92978012ee8f309fdfa",
+    "url": "/static/js/main.b638ce97.chunk.js"
+  },
+  {
+    "revision": "a995fd588ad4eb6e4592",
+    "url": "/static/js/runtime-main.4fc56509.js"
+  },
+  {
+    "revision": "719c708d53106a2cad12b46afb9f26f8",
+    "url": "/static/media/Tangerine.719c708d.svg"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/CitrusHack2020/precache-manifest.7904c74ab80cb4f7becb9cf2240bda93.js"
+  "/precache-manifest.bd325499283b5cdc5d97f297489803f0.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/CitrusHack2020/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
