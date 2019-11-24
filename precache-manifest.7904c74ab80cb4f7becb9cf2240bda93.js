cf18a84ea15929cb9c70e6cf2aaa0d227812eb8f
John Shin <jshin029@ucr.edu>
2019-11-23 18:20:28 -0800

    Updates

---
+++ precache-manifest.7904c74ab80cb4f7becb9cf2240bda93.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1dfdd73ba788f27b141b74aade3295ae",
+    "url": "/CitrusHack2020/index.html"
+  },
+  {
+    "revision": "d781d1f361c870826e06",
+    "url": "/CitrusHack2020/static/css/main.850a195a.chunk.css"
+  },
+  {
+    "revision": "77fc41e91889234c3976",
+    "url": "/CitrusHack2020/static/js/2.54e000c7.chunk.js"
+  },
+  {
+    "revision": "d781d1f361c870826e06",
+    "url": "/CitrusHack2020/static/js/main.4b79cf8b.chunk.js"
+  },
+  {
+    "revision": "8a9dcbb17516db1e083e",
+    "url": "/CitrusHack2020/static/js/runtime-main.fec9978b.js"
+  },
+  {
+    "revision": "719c708d53106a2cad12b46afb9f26f8",
+    "url": "/CitrusHack2020/static/media/Tangerine.719c708d.svg"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/CitrusHack2020/static/media/logo.25bf045c.svg"
+  }
+]);
