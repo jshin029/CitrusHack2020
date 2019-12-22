47f85e8dc39e23c987e5a821676fbf067a94b681
John Shin <jshin029@ucr.edu>
2019-12-21 21:00:48 -0800

    changed to new figma

--- src/App.js
+++ src/App.js
@@ -1,19 +1,26 @@
 import React from 'react';
 import logo from './logo.svg';
+import { Button } from 'antd';
 import './App.css';
 
-const tangerine = require('./assets/Tangerine.svg');
+const tangerine = require('./assets/tangerine.png');
 
 function App() {
   return (
-    <div className="App">
-      <h1 className="app__title">APRIL 24 - 26</h1>
-      <div className="app__image-container">
-        <img className="app__image" src={tangerine}></img>
+    <div className="container-fluid App">
+      <div className="row">
+        <div className="lg-7">
+          <h4 className="App__header-mini">CONNECT . HACK . LEARN .</h4>
+          <h2 className="App__header-main">Citrus Hack 2020</h2>
+          <h3 className="App__header-subMain">Get notified when we launch by joining our mailing list</h3>
+          <div className="App__mobile-container">
+            <button className="App__button-subscribe">Subscribe</button>
+          </div>
+        </div>
+        <div className="lg-5">
+          <img className="App__image" src={tangerine}></img>
+        </div>
       </div>
-      <h1 className="app__title-citrus">CITRUS HACK 2020</h1>
-      <h2 className="app__subTitle">COMING SOON</h2>
-      <h3 style={{color: 'white', textAlign: 'center'}}>citrushack@gmail.com</h3>
     </div>
   );
 }
