20436ef6757a6a555863747b76615f9b10e25d85
John Shin <jshin029@ucr.edu>
2020-01-01 17:43:49 -0800

    updated favicon

--- src/components/Home.js
+++ src/components/Home.js
@@ -31,20 +31,19 @@ class Home extends Component {
         return (
             <div className="container-fluid App">
                 <div className="row">
-                    <div className="lg-7">
+                    <div className="col-lg-7">
                         <h4 className="App__header-mini">CONNECT . HACK . LEARN .</h4>
                         <h2 className="App__header-main">Citrus Hack 2020</h2>
                         <h3 className="App__header-subMain">Get notified when we launch by joining our mailing list</h3>
                         <div className="App__mobile-container">
                             <button className="App__button-subscribe" onClick={this.handleSub}> Subscribe</button>
                         </div>
-          
                     </div>
-
+                    <div className="col-lg-5 container-fluid">
+                      <img className="App__image" src={tangerine}></img>
+                    </div>
                 </div>
-                {/* <div className="lg-5 container-fluid"> */}
-                        <img className="App__image" src={tangerine}></img>
-                    {/* </div> */}
+
             </div>
         );
     }
